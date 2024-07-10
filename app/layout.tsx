9c6d5a35da6dffe0456323def3fe0827d5f1f8c6
namsgit <namanbhulawat@gmail.com>
2024-07-10 15:54:09 +0530

    added metadata tags

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,16 @@
 import '@/app/ui/global.css'
 import {inter} from '@/app/ui/fonts'
+import { Metadata } from 'next';
+
+export const metadata: Metadata = {
+  title: {
+    template: '%s | Acme dashoard', 
+    default: 'Acme dashboard'
+  },
+  description: 'Naman dashboard for customers',
+  metadataBase: new URL('https://next-learn-dashboard.vercel.sh')
+}
+
 export default function RootLayout({
   children,
 }: {
